bff600282dfe715fe07a8f40de1d238331ea8e3b
davidmchale <dmchale@squiz.net>
2022-04-10 14:24:57 +1000

    video

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Gallery.css';
+import './Gallery.scss';
 
 const Gallery = () => (
   <div>
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import './Intro.scss';
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+
+  const [play, setPlay] = useState(false);
+
+  const videoRef = useRef(null);
+
+  const playVideo = () => {
+       setPlay(!play); // get the opposite on click
+        // console.log(play);
+
+        if(play){ // if play === true then set to pause
+         videoRef.current.pause();
+        }else{
+          videoRef.current.play();
+        }
+  }
+
+  
+  
+
+  return(
+    <div className="app__video" onClick={playVideo}>
+         <video
+         src={meal}
+         type="mp4"
+         muted={true}
+         loop={true}
+         controls={false}
+         ref={videoRef}>
+         </video>
+
+         <div className="app__video-overlay flex__center">
+
+        <div className="app__video-overlay_circle flex__center">
+
+        {
+          play ? (<BsPauseFill color="#fff" fontSize={30} />) : (<BsFillPlayFill color="#fff" fontSize={30} />)
+        }
+         
+  
+
+            
+
+        </div>
+      </div>
+      </div>
+  );
+};
 
 export default Intro;
