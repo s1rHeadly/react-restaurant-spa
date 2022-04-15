50985778e9ce05cc70642345afb3d304b70d5a30
davidmchale <dmchale@squiz.net>
2022-04-15 14:11:51 +1000

    gallery finished

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
 import './Gallery.scss';
 
-const Gallery = () => (
-  <div>
+import { SubHeading} from '../../components';
+import { images} from '../../constants';
+
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
+
+
+const Gallery = () => {
+  return(
+    <div>
     Gallery
   </div>
-);
+
+  
+  );
+};
 
 export default Gallery;
