b29b955b049ee83db6283edf04bac345e48c2ba3
davidmchale <dmchale@squiz.net>
2022-05-12 12:18:01 +1000

    fixes3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 
-
+// new react router 6 way of routing
 const element = document.getElementById('root');
 const root = createRoot(element);
 root.render(<StrictMode><App /></StrictMode>);
